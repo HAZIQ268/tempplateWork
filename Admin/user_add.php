966711adaa2e6d04f767c6373817649d5841463a
Mustafa khan <122636626+HareemRehan13@users.noreply.github.com>
2024-04-22 18:14:25 +0500

    php work

--- Admin/user_add.php
+++ Admin/user_add.php
@@ -15,7 +15,7 @@ $result = mysqli_query($conn , $sql);
                     <div class="col-sm-6 p-md-0">
                         <div class="welcome-text">
                             <h4>Hi, welcome back!</h4>
-                            <span class="ml-1">Element</span>
+                            <span class="ml-1">Role</span>
                         </div>
                     </div>
                     <div class="col-sm-6 p-md-0 justify-content-sm-end mt-2 mt-sm-0 d-flex">
@@ -27,17 +27,17 @@ $result = mysqli_query($conn , $sql);
                 </div>
 
 
-                <div class="card">
+                            <div class="card">
                             <div class="card-header">
                                 <h4 class="card-title">User Form</h4>
                             </div>
                             <div class="card-body">
                                 <div class="basic-form">
-                                    <form>
+                                    <form method="POST">
                                     <div class="form-row">
                                             <div class="form-group col-md-12">
                                                 <label>Role</label>
-                                                <select id="inputState" class="form-control">
+                                                <select id="inputState" name="u_r_id" class="form-control">
                                                     <?php
 
                                                     while ($rows =mysqli_fetch_assoc($result)) {
@@ -58,12 +58,11 @@ $result = mysqli_query($conn , $sql);
                                                 <label>Password</label>
                                                 <input type="password" name="password" class="form-control" placeholder="Enter Your Password">
                                             </div>
-                                           
                         
                                         </div>
                                         
                                     
-                                        <button type="submit" class="btn btn-primary">Sign in</button>
+                                        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
                                     </form>
                                 </div>
                             </div>
@@ -71,24 +70,19 @@ $result = mysqli_query($conn , $sql);
                         </div>
                         </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <?php
-include("footer.php");
+            if (isset($_POST['submit'])) {
+            $roleId =$_POST['u_r_id'];
+            $username =$_POST['username'];
+            $password =$_POST['password'];
+
+            $sql="insert into users (username,password,RoleId_FK) values ('$username','$password','$roleId')";
+            $result=mysqli_query($conn,$sql);
+
+            echo 
+                "<script>
+                alert('Your Record has been Inserted');
+                </script>";
+     }
+        include("footer.php");
 ?>
